30d1d8cfd440b686748ff74b161c5717ccfb1e21
Benhur Külzer <benhur.bitpc@gmail.com>
2020-04-20 14:24:47 -0300

    Create/List Transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -12,6 +12,28 @@ interface Balance {
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
     // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (accumulator, transactions) => {
+        switch (transactions.type) {
+          case 'income':
+            accumulator.income += transactions.value;
+            break;
+
+          case 'outcome':
+            accumulator.outcome += transactions.value;
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 }
 
